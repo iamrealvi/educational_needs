00d8fd4b372159162bfac8b7effb8679faee932d
ticklere <iamrealvi@gmail.com>
2022-02-23 21:57:10 +0700

    cell divide by hours

---
+++ second_block/cell_divide.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+  
+    int a;
+    int res;
+    int cell;
+    scanf("%d", &a);
+    cell = a / 3;
+    res = pow(2, cell);
+    printf("%d", res);
+
+
+
+    return 0;
+}
