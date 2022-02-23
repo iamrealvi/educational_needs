ef30b687307d640af840b3c7d26b8cd601bb75ed
ticklere <iamrealvi@gmail.com>
2022-02-23 15:52:00 +0700

    il-96 from point to point radius and hours

---
+++ second_block/radius.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+  long int planet_radius;
+  scanf("%d", &planet_radius);
+
+  long int disk_radius;
+  long int hours_q;
+ 
+  disk_radius = planet_radius * 2;
+  hours_q = (planet_radius * 4) / 900;
+  
+  printf("%ld %ld", disk_radius, hours_q);
+
+  return 0;
+}
