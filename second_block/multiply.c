a0af4a5fedf42c311c70220782bf4d0fa33f0760
ticklere <iamrealvi@gmail.com>
2022-02-23 16:15:07 +0700

    5 digits with zero`s - multiplies

---
+++ second_block/multiply.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    
+    int a1,a2,a3,a4,a5;
+    int res;
+    scanf("%d%d%d%d%d", &a1,&a2,&a3,&a4,&a5);
+    res = pow(a1, 2) + pow(a2, 2) + pow(a3, 2) + pow(a4, 2) + pow(a5, 2);
+    printf("%d", res);
+
+    return 0;
+}
