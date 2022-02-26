a73afc96c5a69094fdc58cf31ff8ba440310c67a
ticklere <iamrealvi@gmail.com>
2022-02-26 20:50:52 +0700

    calc by switch statements

---
+++ fourth_block/switch_calc.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+  char c;
+  int a,b;
+  double result;
+  if (scanf("%c%d%d", &c, &a, &b) && b != 0) {
+    switch(c) {
+        case '+':
+            result = a + b;
+            printf("%.2lf", result);
+            break;
+        case '-':
+            result = a - b;
+            printf("%.2lf", result);
+            break;
+        case '*':
+            result = a * b;
+            printf("%.2lf", result);
+            break;
+        case '/':
+            result = a / b;
+            printf("%.2lf", result);
+            break;
+        default: 
+            printf("ERROR!");
+            break;
+  }
+} else {
+    printf("ERROR!");
+}
+  return 0;
+}
