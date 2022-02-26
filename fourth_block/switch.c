1521ec8730fe2ea821486c5fb4008605d2433402
ticklere <iamrealvi@gmail.com>
2022-02-26 20:38:25 +0700

    strange quest btw

---
+++ fourth_block/switch.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(void) {
+  setlocale(LC_ALL, "");
+  char s;  
+  scanf("%c",&s);
+
+  switch (s) {
+    case 'l': 
+    printf("коня потеряешь, себя спасёшь!\n"); 
+    break;
+    case 'f': 
+    printf("и себя и коня потеряешь!\n"); 
+    break;
+    case 'r': 
+    printf("себя потеряешь, коня спасёшь!\n");
+    break;
+    default: break;
+  }
+
+  return 0;
+}
