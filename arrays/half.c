5f0e95605f25bc228a8e10dda45fd11e93f56d23
ticklere <iamrealvi@gmail.com>
2022-03-07 18:30:55 +0700

    reverse arr

---
+++ arrays/half.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+
+    int N;
+    scanf("%d", &N);
+    int arr[N];
+
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = (N / 2) - 1; i >= 0; i--) {
+       printf("%d ", arr[i]);
+    }
+
+    for (int i = 0; i < N / 2; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
